6c875ba3b37bec830bd5b7a26f731ebf6a1d201e
Chris Phillips <chris.is.phillips@gmail.com>
2020-02-20 15:59:52 +0000

    JC-8 Add json and urlencoded middleware from body-parser

--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
 import express from "express"
 import cors from "cors"
+import { json, urlencoded } from "body-parser"
 
 const port = 3000
 const app = express()
 
 app.use(cors())
+app.use(json())
+app.use(urlencoded({ extended: true }))
 
 app.listen(port, () => {
   console.log(`Express server running on port ${port}`)
