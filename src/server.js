be237fcdca8ca7355fe4597b779c5ddcc4410748
Chris Phillips <chris.is.phillips@gmail.com>
2020-02-20 16:02:10 +0000

    JC-9 Add morgan middleware

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import { json, urlencoded } from "body-parser"
+import morgan from "morgan"
 
 const port = 3000
 const app = express()
@@ -8,6 +9,7 @@ const app = express()
 app.use(cors())
 app.use(json())
 app.use(urlencoded({ extended: true }))
+app.use(morgan("dev"))
 
 app.listen(port, () => {
   console.log(`Express server running on port ${port}`)
