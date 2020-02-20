5d36aa082f8548d81f2f2b539801249782b1d93f
Chris Phillips <chris.is.phillips@gmail.com>
2020-02-20 15:59:03 +0000

    JC-7 Add cors middleware

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
 import express from "express"
+import cors from "cors"
 
 const port = 3000
 const app = express()
+
+app.use(cors())
+
 app.listen(port, () => {
   console.log(`Express server running on port ${port}`)
 })
