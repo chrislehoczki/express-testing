a476593a8433447dce7fbe7e20d4fdc5d3fac67d
Chris Phillips <chris.is.phillips@gmail.com>
2020-02-21 13:18:38 +0000

    Create user controller with mock database
    Added tests for controller, and post request for creating a new user - tested with supertest.

--- src/resources/user/user.router.js
+++ src/resources/user/user.router.js
@@ -1,9 +1,13 @@
 import express from "express"
-
+import { getUsers, addUser } from "./user.controller.js"
 const userRouter = express.Router()
 
-userRouter.route("/").get((req, res) => {
-  res.json({ id: "12345" })
-})
+userRouter
+  .route("/")
+  .get((req, res) => res.status(200).json(getUsers()))
+  .post((req, res) => {
+    addUser(req.body)
+    res.status(200).send({ status: "OK" })
+  })
 
 export default userRouter
