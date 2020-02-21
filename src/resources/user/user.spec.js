61265f640fe0c290a2f46d34f9f52169cc58020e
Chris Phillips <chris.is.phillips@gmail.com>
2020-02-21 07:16:31 +0000

    JC-10 Create test for get "/user"
    Added test suite using jest, test script in package.json, refactored app and start script to allow supertest to consume express app

---
+++ src/resources/user/user.spec.js
@@ -0,0 +1,14 @@
+import { app } from "../../server"
+const request = require("supertest")
+
+describe("userRouter", () => {
+  it("Should return 200 and json sample for /user", () => {
+    request(app)
+      .get("/user")
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .end(function(err, res) {
+        if (err) throw err
+      })
+  })
+})
--- src/server.js
+++ src/server.js
@@ -1,16 +1,16 @@
 import express from "express"
-import cors from "cors"
 import { json, urlencoded } from "body-parser"
 import morgan from "morgan"
 
 const port = 3000
-const app = express()
+export const app = express()
 
-app.use(cors())
 app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan("dev"))
 
-app.listen(port, () => {
-  console.log(`Express server running on port ${port}`)
-})
+export const start = () => {
+  app.listen(port, () => {
+    console.log(`Express server running on port ${port}`)
+  })
+}
