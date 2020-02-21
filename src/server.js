4fa4e4f97d464f4cde05f59a08c014ce838d432f
Chris Phillips <chris.is.phillips@gmail.com>
2020-02-21 07:22:08 +0000

    JC-10 Create router in separate file and mount to app

--- src/server.js
+++ src/server.js
@@ -2,12 +2,16 @@ import express from "express"
 import { json, urlencoded } from "body-parser"
 import morgan from "morgan"
 
+import userRouter from "./resources/user/user.router"
+
 const port = 3000
 export const app = express()
 
 app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan("dev"))
+
+app.use("/user", userRouter)
 
 export const start = () => {
   app.listen(port, () => {
